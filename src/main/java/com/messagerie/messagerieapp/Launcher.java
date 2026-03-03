d0622f0a72f10974b4261ea0fcb45172d21d8fe8
Mamadou Mbaye <mamadoumbaye@groupeisi.com>
2026-03-03 00:01:17 +0000

    Creation des packages

---
+++ src/main/java/com/messagerie/messagerieapp/Launcher.java
@@ -0,0 +1,9 @@
+package com.messagerie.messagerieapp;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
